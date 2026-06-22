4907c9e631831ddd27bf675060662954efb1f428
Asanthi Kulasinghe <kaushie.kk@gmail.com>
2026-06-22 12:23:07 -0700

    Adding changes

--- tests/datePickers.spec.ts
+++ tests/datePickers.spec.ts
@@ -11,9 +11,10 @@ test.beforeEach(async ({ page }) => {
 test('Select the desired date in calendar', async ({ page }) => {
     await page.getByRole('row', { name: "Harold Davis" }).getByRole('link').click()
     await page.getByRole('button', { name: "Add New Pet" }).click()
-    await expect(page.getByRole('textbox', { name: "Name" })).toBeVisible()
+
+    await expect(page.locator('#name + .glyphicon')).toHaveClass(/glyphicon-remove/)
     await page.getByRole('textbox', { name: "name" }).fill('Tom')
-    await expect(page.locator('span.glyphicon-ok')).toBeVisible()
+    await expect(page.locator('#name + .glyphicon')).toHaveClass(/glyphicon-ok/)
 
     await page.getByRole('button', { name: "Open calendar" }).click()
     await page.getByRole('button', { name: 'Choose month and year' }).click()
@@ -27,57 +28,64 @@ test('Select the desired date in calendar', async ({ page }) => {
     await page.getByRole('combobox').selectOption('dog')
     await page.getByRole('button', { name: "Save Pet" }).click()
 
-    const petInfo = page.locator('app-pet-list')
-    await expect(petInfo.getByText('Tom')).toBeVisible()
-    await expect(petInfo.getByText('2014-05-02')).toBeVisible()
-    await expect(petInfo.getByText('dog')).toBeVisible()
-    await petInfo.filter({ hasText: 'Tom' }).getByRole('button', { name: "Delete Pet" }).click()
-    await expect(petInfo.filter({ hasText: 'Tom' })).not.toBeVisible()
+    const petInfoOfOwner = page.locator('app-pet-list')
+
+    await expect(petInfoOfOwner.last().locator('dd').first()).toHaveText('Tom')
+    await expect(petInfoOfOwner.last().locator('dd').nth(1)).toHaveText('2014-05-02')
+    await expect(petInfoOfOwner.last().locator('dd').nth(2)).toHaveText('dog')
+
+    await petInfoOfOwner.filter({ hasText: 'Tom' }).getByRole('button', { name: "Delete Pet" }).click()
+    await expect(petInfoOfOwner.filter({ hasText: 'Tom' })).not.toBeVisible()
 
 })
 
-test.only('Select the dates of visits and validate dates order', async ({ page }) => {
-    
+test('Select the dates of visits and validate dates order', async ({ page }) => {
+
     await page.getByRole('row', { name: "Jean Coleman" }).getByRole('link').click()
-    const petSamantha = page.locator('app-pet-list').filter({ hasText: 'Samantha' })
-    await petSamantha.getByRole('button', { name: "Add Visit" }).click()
+    const infoOfPetSamantha = page.locator('app-pet-list', { hasText: 'Samantha' })
+    await infoOfPetSamantha.getByRole('button', { name: "Add Visit" }).click()
     // Add visit
     await expect(page.getByRole('heading')).toHaveText('New Visit')
     await expect(page.getByRole('row').locator('td').nth(0)).toHaveText('Samantha')
     await expect(page.getByRole('row').locator('td').nth(3)).toHaveText('Jean Coleman')
     await page.getByRole('button', { name: "Open calendar" }).click()
-    await page.locator('.mat-calendar-body-today').click()
-    await expect(page.locator('input[name="date"]')).toHaveValue(/^\d{4}\/\d{2}\/\d{2}$/)
+
+    const today = new Date().getDate()
+    await page.getByRole('gridcell', { name: String(today) }).click()
+    const todaysDateString = `${new Date().getFullYear()}/${String(new Date().getMonth() + 1).padStart(2, '0')}/${String(new Date().getDate()).padStart(2, '0')}`
+    await expect(page.locator('input[name="date"]')).toHaveValue(todaysDateString)
+
+
     await page.locator('input[name="description"]').fill('dermatologists visit')
     await page.getByRole('button', { name: "Add Visit" }).click()
 
-    const visitsOfSamantha = petSamantha.locator('app-visit-list')
+    const visitsOfSamantha = infoOfPetSamantha.locator('app-visit-list')
     await expect(visitsOfSamantha.locator('tr').nth(1).locator('td').first()).toHaveText(/^\d{4}-\d{2}-\d{2}$/)
 
     //Add another visit                                                
-    await petSamantha.getByRole('button', { name: "Add Visit" }).click()
+    await infoOfPetSamantha.getByRole('button', { name: "Add Visit" }).click()
     await page.getByRole('button', { name: "Open calendar" }).click()
 
     const date = new Date()
-    date.setDate(date.getDate() - 45)
-    const day = String(date.getDate())
-    const monthShort = date.toLocaleString('en-US', { month: 'short' }).toUpperCase()
-    const year = date.getFullYear().toString()
-    await page.getByRole('button', { name: 'Choose month and year' }).click();
-    await page.getByText(year, { exact: true }).click()
-    await page.getByText(monthShort, { exact: true }).click()
-    await page.getByText(day, { exact: true }).click()
+    date.setDate(today - 45)
+    const pastVisitDay = String(date.getDate())
+    const pastVisitMonthShort = date.toLocaleString('en-US', { month: 'short' }).toUpperCase()
+    const pastVisitYear = date.getFullYear().toString()
+    await page.getByRole('button', { name: 'Choose month and year' }).click()
+    await page.getByText(pastVisitYear, { exact: true }).click()
+    await page.getByText(pastVisitMonthShort, { exact: true }).click()
+    await page.getByText(pastVisitDay, { exact: true }).click()
     await page.locator('input[name="description"]').fill('massage therapy')
     await page.getByRole('button', { name: "Add Visit" }).click()
 
     //Ensure visit date chronological order
-    const newDateText = await visitsOfSamantha.locator('tr').nth(1).locator('td').first().innerText()
-    const oldDateText = await visitsOfSamantha.locator('tr').nth(2).locator('td').first().innerText()
+    const recenetVisitDateText = await visitsOfSamantha.locator('tr').nth(1).locator('td').first().innerText()
+    const pastVisitDateTextt = await visitsOfSamantha.locator('tr').nth(2).locator('td').first().innerText()
 
-    const newDate = new Date(newDateText)
-    const oldDate = new Date(oldDateText)
+    const recentVisitDate = new Date(recenetVisitDateText)
+    const pastVisitDate = new Date(pastVisitDateTextt)
 
-    expect(oldDate < newDate).toBeTruthy()
+    expect(pastVisitDate < recentVisitDate).toBeTruthy()
 
     //Delete added visits
     await visitsOfSamantha.getByRole('row').filter({ hasText: "dermatologists visit" }).getByRole('button', { name: 'Delete Visit' }).click()
