9f8d06621f8281d561e3eabfb1de188069cfcabf
Asanthi Kulasinghe <kaushie.kk@gmail.com>
2026-07-01 10:53:22 -0700

    Intercepting API response - practice assignment

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import owners from '../test-data/owners.json'
+import sharonSpecialties from '../test-data/sharonSpecialties.json'
 
 test.beforeEach(async ({ page }) => {
 
@@ -14,7 +15,19 @@ test.beforeEach(async ({ page }) => {
         })
     })
 
+    await page.route('*/**/api/vets', async route => {
+        const response = await route.fetch()
+        const vets: any[] = await response.json()
+        const sharon = vets.find(vet => vet.id === 3493)
+        if (sharon) {
+            sharon.specialties = sharonSpecialties
+        }
+        await route.fulfill({
+            response, json: vets
+        })
+    })
 })
+
 
 test('Mocking API request', async ({ page }) => {
 
@@ -49,4 +62,15 @@ test('Mocking API request', async ({ page }) => {
     await expect(ownerPetLocator.first().locator('app-visit-list td:first-child')).toHaveCount(10)
 })
 
+test('Intercept API response', async ({ page }) => {
+
+    await page.goto('/')
+    await page.getByText('Veterinarians').click()
+    await page.getByText('All').click()
+
+    for (const specialty of sharonSpecialties) {
+        await expect(page.getByRole('row', { name: 'Sharon Jenkins' })).toContainText(specialty.name)
+    }
+
+})
 
