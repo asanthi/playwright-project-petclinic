0f3d84addb0f9158bfa45470f45de5e140fe394a
Asanthi Kulasinghe <kaushie.kk@gmail.com>
2026-07-03 11:17:04 -0700

    Adding changes

--- tests/api-requests.spec.ts
+++ tests/api-requests.spec.ts
@@ -1,20 +1,14 @@
 import { test, expect } from '@playwright/test'
-import { spec } from 'node:test/reporters'
-
-const token = process.env.ACCESS_TOKEN!
 
 test('Validation of delete specialty', async ({ page, request }) => {
 
     //Create specialty
-    const response = await request.post('https://petclinic-api.bondaracademy.com/petclinic/api/specialties', {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
+    const createSpecialtyResponse = await request.post('https://petclinic-api.bondaracademy.com/petclinic/api/specialties', {
         data: {
             name: 'api testing expert'
         }
     })
-    expect(response.status()).toBe(201)
+    expect(createSpecialtyResponse.status()).toBe(201)
 
     //Assert availability of specialty
     await page.goto('/')
@@ -31,19 +25,16 @@ test('Add and delete veterinarian', async ({ page, request }) => {
     //Add Vet and assiggn specialty
     const firstName = 'Asanthi'
     const lastName = "Kulasinghe"
-    const vetApi = "https://petclinic-api.bondaracademy.com/petclinic/api/vets"
-    const createVetResponse = await request.post(vetApi, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
+    const apiBaseUrl = "https://petclinic-api.bondaracademy.com/petclinic/api"
+    const createVetResponse = await request.post(`${apiBaseUrl}/vets`, {
         data: { "firstName": firstName, "lastName": lastName, "id": null, "specialties": [] }
     })
 
-    const createVetResponseBody = await createVetResponse.json()
-    const vetID = createVetResponseBody.id
+    const createVetResponseJSON = await createVetResponse.json()
+    const vetID = createVetResponseJSON.id
     expect(createVetResponse.status()).toBe(201)
-    expect(createVetResponseBody.firstName).toBe(firstName)
-    expect(createVetResponseBody.lastName).toBe(lastName)
+    expect(createVetResponseJSON.firstName).toBe(firstName)
+    expect(createVetResponseJSON.lastName).toBe(lastName)
 
     await page.goto('/')
     await page.getByText('Veterinarians').click()
@@ -62,24 +53,12 @@ test('Add and delete veterinarian', async ({ page, request }) => {
     await expect(newVetRow.locator('td').nth(1)).toHaveText('dentistry')
 
     //Delete Vet
-
-    const delVetResponse = await request.delete(`${vetApi}/${vetID}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-
+    const delVetResponse = await request.delete(`${apiBaseUrl}/vets/${vetID}`)
     expect(delVetResponse.status()).toBe(204)
 
     //Assert the deleted vet is not displayed in the list
-    const vetListResponse = await request.get(vetApi, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-
+    const vetListResponse = await request.get(`${apiBaseUrl}/vets`)
     const vetListResponseBody = await vetListResponse.json()
-
     expect(vetListResponseBody).not.toEqual(
         expect.arrayContaining([
             expect.objectContaining({ id: vetID })
@@ -91,11 +70,8 @@ test('New specialty is displayed', async ({ page, request }) => {
 
     //Create specialty
     const newSpecialty = "api testing ninja"
-    const specialtyApi = "https://petclinic-api.bondaracademy.com/petclinic/api/specialties"
-    const createSpecialtyResponse = await request.post(specialtyApi, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
+    const apiBaseUrl = "https://petclinic-api.bondaracademy.com/petclinic/api"
+    const createSpecialtyResponse = await request.post(`${apiBaseUrl}/specialties`, {
         data: {
             name: newSpecialty
         }
@@ -104,17 +80,19 @@ test('New specialty is displayed', async ({ page, request }) => {
     const specialtyID = createSpecialtyResponseBody.id
     expect(createSpecialtyResponse.status()).toBe(201)
 
+    //Retrieve specialty ID for surgery
+    const listSpecialtiesResponse = await request.get(`${apiBaseUrl}/specialties`)
+    const specialtyList: any[] = await listSpecialtiesResponse.json()
+    const surgery = specialtyList.find(specialty => specialty.name == "surgery")
+    const surgeryID = surgery.id
+
     //Create new Vet 
     const firstName = "Dilushi"
     const lastName = "Kulasinghe"
-    const vetApi = "https://petclinic-api.bondaracademy.com/petclinic/api/vets"
-    const createVetResponse = await request.post(vetApi, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
+    const createVetResponse = await request.post(`${apiBaseUrl}/vets`, {
         data: {
             "firstName": firstName, "lastName": lastName, "id": null, "specialties": [{
-                id: 5285, name: "surgery"
+                id: surgeryID, name: "surgery"
             }]
         }
     })
@@ -138,23 +116,16 @@ test('New specialty is displayed', async ({ page, request }) => {
     await expect(newVetRow.locator('td').nth(1)).toHaveText(newSpecialty)
 
     //Delete vet
-    const delVetResponse = await request.delete(`${vetApi}/${vetID}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
+    const delVetResponse = await request.delete(`${apiBaseUrl}/vets/${vetID}`)
     expect(delVetResponse.status()).toBe(204)
 
     //Delete specialty 
-    const delSpecialtyResponse = await request.delete(`${specialtyApi}/${specialtyID}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
+    const delSpecialtyResponse = await request.delete(`${apiBaseUrl}/specialties/${specialtyID}`)
     expect(delSpecialtyResponse.status()).toBe(204)
 
     await page.goto('/')
     await page.getByText('Specialties').click()
+    await page.waitForResponse('**/specialties')
     await expect(page.getByRole('row', { name: newSpecialty })).not.toBeVisible()
 
 })
